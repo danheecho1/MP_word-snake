897c63ffbb456c3385d456145cda0b9e65890e69
Danny Cho <danheecho@gmail.com>
2023-06-10 01:10:28 -0700

    Moved the snake image to its own component. Finished HTML and CSS for home and about pages.

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,11 @@
 import React from "react";
-import Image from "next/image";
-import snake from "../public/snake.PNG"
 import styles from "./page.module.css";
+import { Snake } from "@/components";
 
 const Home = () => {
 	return (
 		<>
-			<Image
-				src={snake}
-				alt="Drawing of a word snake"
-				width={750}
-				height={400}
-        className={styles.snakeImg}
-			/>
+			<Snake />
       <p className={styles.snakeQuote}>"Give me a long word that starts with a letter that my word ends with!" - Word Snake</p>
       <form className={styles.form}>
         <label className={styles.label}>Name: </label>
---
+++ components/Snake.jsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import snake from "../public/snake.PNG";
+import styles from "./styles.module.css"
+import React from "react";
+
+const Snake = () => {
+	return (
+		<Image
+			src={snake}
+			alt="Drawing of a word snake"
+			width={750}
+			height={400}
+			className={styles.snakeImg}
+		/>
+	);
+};
+
+export default Snake;
