e28ab441aaaede78e8329c1be9aac3a132a9c6f5
Danny Cho <danheecho@gmail.com>
2023-06-10 02:26:05 -0700

    Minor styling changes for links. Nav bar menus updated to 'About' only. Play page HTML and CSS done.

--- app/play/page.jsx
+++ app/play/page.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import styles from "./page.module.css";
 
 const Play = () => {
-  return (
-    <div>Play</div>
-  )
-}
+	return (
+		<>
+			<div className={styles.currentInfoDiv}>
+				<div className={styles.currentScore}>
+					<p>
+						Current score: <span>34</span>
+					</p>
+				</div>
+				<div className={styles.currentWord}>
+					<p>
+						Current word: <span>nice</span>
+					</p>
+				</div>
+			</div>
+			<div className={styles.timer}>
+				<p>
+					Time remaining: <span>8</span>
+				</p>
+			</div>
+			<form className={styles.form}>
+				<label className={styles.label}>New word:</label>
+				<input type="text" className={styles.input} />
+				<button className={styles.button}>Submit</button>
+			</form>
+			<div className={styles.wordSnakeBody}>
+				<span className={styles.snakeHead}>🐍</span>
+				<p className={styles.snakeTail}>
+					banana - ape - eggs - sponge - east - trim - man - nice - ape - eggs - sponge - east - trim - man - nice - ape - eggs - sponge - east - trim - man - nice - ape - eggs - sponge - east - trim - man - nice - ape - eggs - sponge - east - trim - man - nice - ape - eggs - sponge - east - trim - man - nice - ape - eggs - sponge - east - trim - man - nice - ape - eggs - sponge - east - trim - man - nice - ape - eggs - sponge - east - trim - man - nice - ape - eggs - sponge - east - trim - man - nice - ape - eggs - sponge - east - trim - man - nice - ape - eggs - sponge - east - trim - man - nice - ape - eggs - sponge - east - trim - man - nice - ape - eggs - sponge - east - trim - man - nice - ape - eggs - sponge - east - trim - man - nice - ape - eggs - sponge - east - trim - man - nice- ape - eggs - sponge - east - trim - man - nice - ape - eggs - sponge - east - trim - man - nice - ape - eggs - sponge - east - trim - man - nice - ape - eggs - sponge - east - trim - man - nice - ape - eggs - sponge - east - trim - man - nice - ape - eggs - sponge - east - trim - man - nice - ape - eggs - sponge - east - trim - man - nice - ape - eggs - sponge - east - trim - man - nice - ape - eggs - sponge - east - trim - man - nice  ...
+				</p>
+			</div>
+		</>
+	);
+};
 
-export default Play
+export default Play;
