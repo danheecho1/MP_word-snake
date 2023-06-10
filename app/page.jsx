eccc9066c6a21d9a7934a1cc7aa13e0334777d24
Danny Cho <danheecho@gmail.com>
2023-06-09 23:55:38 -0700

    Set up app router and confirmed layout and pages work as intended.

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import snake from "../public/snake.PNG"
+import styles from "./page.module.css";
 
 const Home = () => {
-  return (
-    <div>This is main app directory's page jsx</div>
-  )
-}
+	return (
+		<>
+			<Image
+				src={snake}
+				alt="Drawing of a word snake"
+				width={500}
+				height={500}
+			/>
+      <p>"Give me a long word that starts with a letter that my word ends with!" - Word Snake</p>
+      <form>
+        <label>Your name: </label>
+        <input type="text" />
+        <button>Start</button>
+      </form>
+		</>
+	);
+};
 
-export default Home
+export default Home;
