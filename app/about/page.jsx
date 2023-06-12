6c8f30eb0cc323aa4fb6cf15efc293abb182f2dc
Danny Cho <danheecho@gmail.com>
2023-06-11 18:58:24 -0700

    Minor text updates. Also tested the dictionary API to make sure it still works.

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,5 +1,5 @@
 import { Snake } from "@/components";
-import Link from "next/link"
+import Link from "next/link";
 import styles from "./page.module.css";
 import React from "react";
 
@@ -15,20 +15,24 @@ const About = () => {
 					the current word ends with an 'e', and your new word starts
 					with an 'e' as well. Then, the word you provide becomes the
 					current word, and the game continues until you cannot
-					provide the next word within 10 seconds. Entering an invalid word also ends the game.
+					provide the next word within 10 seconds.
 				</p>
 				<p className={styles.description}>
 					Your word will be considered valid if it is found in
-					https://dictionaryapi.dev. This includes nouns (singular and plural), verbs, adjectives, adverbs, conjunctions, prepositions, and so on, but not pronouns. Entering an invalid word will
-					also end the game instantly, so no guessing or making up
-					random words here.
+					https://dictionaryapi.dev. This includes nouns (singular and
+					plural), verbs, adjectives, adverbs, conjunctions,
+					prepositions, and so on, but not pronouns. Entering an
+					invalid word will also end the game instantly, so no
+					guessing or making up random words here.
 				</p>
 				<p className={styles.description}>
 					Your total score will equal to the total number of letters
 					provided multiplied by the total number of words provided.
 				</p>
 			</div>
-      <Link href="/" className={styles.linkToPlay}>Click here if your brain is ready to play</Link>
+			<Link href="/" className={styles.linkToPlay}>
+				Click here if your brain is ready to play
+			</Link>
 		</>
 	);
 };
