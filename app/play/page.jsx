ead08e1b68be227a27a1f7dc2d58ac57e1314dd2
Danny Cho <danheecho@gmail.com>
2023-06-14 22:39:56 -0700

    Added some logics for ending the game: time over, not a word, and a word that does not start with the correct letter.

--- app/play/page.jsx
+++ app/play/page.jsx
@@ -5,7 +5,6 @@ import { useRouter } from "next/navigation";
 import styles from "./page.module.css";
 
 const Play = () => {
-	console.log(`this is in the session: ${sessionStorage}`);
 	const [timeLeft, setTimeLeft] = useState(5);
 	const [newWord, setNewWord] = useState("");
 	const [wordBank, setWordBank] = useState([]);
@@ -44,25 +43,65 @@ const Play = () => {
 		sessionStorage.setItem("score", score);
 		sessionStorage.setItem("numberOfWords", wordBank.length - 1);
 		router.push("/result");
+		alert("You took too long!")
 	}
 
 	function calculateScore(currentWord, remainingSeconds) {
 		return currentWord.length * remainingSeconds;
 	}
 
-	function handleSubmit(e) {
+	async function validateWord(word) {
+		// Making sure the new word submitted starts with the letter that the previous word ended with
+		const lastWord = wordBank[wordBank.length - 1]
+		if(word[0] === lastWord[lastWord.length - 1]) {
+			// Checking that the word does exist in dictionary
+			const fetchWord = async (word) => {
+				const response = await fetch(
+					`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+				);
+				const result = await response.json();
+				return result[0];
+			};
+			try {
+				const response = await fetchWord(word);
+				if(response === undefined) {
+					return false;
+				} else {
+					return true;
+				}
+			} catch (error) {
+				console.error(error);
+				return false;
+			}
+		} else {
+			sessionStorage.setItem("score", score);
+			sessionStorage.setItem("numberOfWords", wordBank.length - 1);
+			router.push("/result");
+			alert("You submitted a word(?) that doesn't start with the letter that the previous word ended with!")
+		}
+	}
+
+	async function handleSubmit(e) {
 		e.preventDefault();
-		// Add submitted word to the word bank
-		setWordBank((currentWordBank) => {
-			return [...currentWordBank, newWord];
-		});
-		// Reset the timer back to 10 seconds
-		setTimeLeft(5);
-		setNewWord("");
-		// Calculate and update the score
-		setScore((currentScore) => {
-			return currentScore + calculateScore(newWord, timeLeft);
-		});
+		const isValid = await validateWord(newWord)
+		if (isValid) {
+			// Add submitted word to the word bank
+			setWordBank((currentWordBank) => {
+				return [...currentWordBank, newWord];
+			});
+			// Reset the timer back to 10 seconds
+			setTimeLeft(5);
+			setNewWord("");
+			// Calculate and update the score
+			setScore((currentScore) => {
+				return currentScore + calculateScore(newWord, timeLeft);
+			});
+		} else {
+			sessionStorage.setItem("score", score);
+			sessionStorage.setItem("numberOfWords", wordBank.length - 1);
+			router.push("/result");
+			alert("The word you submitted doesn't exist!")
+		}
 	}
 
 	return (
