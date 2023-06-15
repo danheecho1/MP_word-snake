43aa4b89d54681255f5f197c63d7a420fa636197
Danny Cho <danheecho@gmail.com>
2023-06-15 02:22:09 -0700

    Updated back to image as source URL instead of module.

--- components/Snake.jsx
+++ components/Snake.jsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
-import snake from "../public/snake.png"
 import styles from "./styles.module.css"
 import React from "react";
 
 const Snake = () => {
 	return (
 		<Image
-			src={snake}
+			src='/snake.png'
 			alt="Drawing of a word snake"
 			width={750}
 			height={400}
