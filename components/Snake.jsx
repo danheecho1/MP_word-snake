62f4aba1d729f8437c32c2a76b233f3d8c16a355
Danny Cho <danheecho@gmail.com>
2023-06-15 02:30:22 -0700

    Back to module.

--- components/Snake.jsx
+++ components/Snake.jsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import styles from "./styles.module.css"
+import snake from "../public/snake.png"
 import React from "react";
 
 const Snake = () => {
 	return (
 		<Image
-			src='/snake.png'
+			src={snake}
 			alt="Drawing of a word snake"
 			width={750}
 			height={400}
